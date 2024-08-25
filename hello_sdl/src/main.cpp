7f16c11ac7b0325ff5566d4bae2bf6886fda1225
David Brown <d.brown@bigdavedev.com>
2024-08-25 16:28:08 +0200

    Make lauching easier on Debian systems

--- hello_sdl/src/main.cpp
+++ hello_sdl/src/main.cpp
@@ -2,19 +2,20 @@
 // SPDX-License-Identifier: MPL-2.0
 #include "ptb/hello/hello.hpp"
 
+#include "SDL3/SDL_error.h"
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_main.h"
 #include "SDL3/SDL_messagebox.h"
-#include "SDL3/SDL_error.h"
 
-#include <format>
 #include <iostream>
+
+#include "fmt/core.h"
 
 int main(int /*argc*/, char** /*argv*/)
 {
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
 	{
-		std::cerr << std::format("Error initialising SDL: {}\n",
+		std::cerr << fmt::format("Error initialising SDL: {}\n",
 		                         SDL_GetError());
 		return -1;
 	}
