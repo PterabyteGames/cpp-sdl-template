c192ad20685c19c2a6be64f7bf3aa5e82aab4a1d
David Brown <d.brown@bigdavedev.com>
2024-12-12 10:59:43 +0100

    Remove fmt as a dependency

--- hello_sdl/src/main.cpp
+++ hello_sdl/src/main.cpp
@@ -7,15 +7,14 @@
 #include "SDL3/SDL_main.h"
 #include "SDL3/SDL_messagebox.h"
 
+#include <format>
 #include <iostream>
-
-#include "fmt/core.h"
 
 int main(int /*argc*/, char** /*argv*/)
 {
 	if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
 	{
-		std::cerr << fmt::format("Error initialising SDL: {}\n",
+		std::cerr << std::format("Error initialising SDL: {}\n",
 		                         SDL_GetError());
 		return -1;
 	}
