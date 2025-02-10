084e389c29fb23581a8ea2b6a8d87e1230150f9a
David Brown <d.brown@bigdavedev.com>
2025-02-10 19:32:08 +0100

    Split packiging scripts in to smaller modules

--- tools/cmake/InstallBuildTools.cmake
+++ tools/cmake/InstallBuildTools.cmake
@@ -6,9 +6,21 @@ function (install_ninja)
 	set (NINJA_REPOSITORY https://github.com/ninja-build/ninja/releases/download)
 	set (NINJA_VERSION "v1.12.1")
 	string (TOLOWER "${CMAKE_HOST_SYSTEM_NAME}" host)
-	FetchContent_Declare (ninja.Windows URL ${NINJA_REPOSITORY}/${NINJA_VERSION}/ninja-win.zip)
-	FetchContent_Declare (ninja.Darwin URL ${NINJA_REPOSITORY}/${NINJA_VERSION}/ninja-mac.zip)
-	FetchContent_Declare (ninja.Linux URL ${NINJA_REPOSITORY}/${NINJA_VERSION}/ninja-linux.zip)
+	FetchContent_Declare (
+		ninja.Windows
+		URL ${NINJA_REPOSITORY}/${NINJA_VERSION}/ninja-win.zip
+		DOWNLOAD_NO_PROGRESS TRUE
+	)
+	FetchContent_Declare (
+		ninja.Darwin
+		URL ${NINJA_REPOSITORY}/${NINJA_VERSION}/ninja-mac.zip
+		DOWNLOAD_NO_PROGRESS TRUE
+	)
+	FetchContent_Declare (
+		ninja.Linux
+		URL ${NINJA_REPOSITORY}/${NINJA_VERSION}/ninja-linux.zip
+		DOWNLOAD_NO_PROGRESS TRUE
+	)
 	FetchContent_MakeAvailable (ninja.${CMAKE_HOST_SYSTEM_NAME})
 	find_program (
 		CMAKE_MAKE_PROGRAM
